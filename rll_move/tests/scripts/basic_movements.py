5469383418e9221c3d7ec1c2eb375452f5be7cb8
Mark Weinreuter <uieai@student.kit.edu>
2019-09-23 11:41:32 +0200

    Setup rostest for the rll_move package and add some basic tests
    Tests can be run with Gitlab CI.
    
    * some CMakeLists.txts had to be adjusted to correctly install programs and config files
    * added a test.py to the rll_move_client to assist in writing tests that require an RLLMoveClient setup
      * see move_iface.test on how to setup a launch file to start the simulation environment
      * see test_main.py on how to configure the RLLMoveClient to run tests and trigger the project execution
      * this allows to write integration tests to test the services provided by rll_move
    * added a tests folder to rll_move which contains test scripts and launch files

---
+++ rll_move/tests/scripts/basic_movements.py
@@ -0,0 +1,70 @@
+#! /usr/bin/env python
+#
+# This file is part of the Robot Learning Lab Move Client
+#
+# Copyright (C) 2019 Mark Weinreuter <uieai@student.kit.edu>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from geometry_msgs.msg import Pose, Point
+
+from rll_move_client.test import TestCaseWithRLLMoveClient
+from rll_move_client.error import RLLErrorCode
+
+
+class TestBasicMovements(TestCaseWithRLLMoveClient):
+
+    def __init__(self, *args, **kwargs):
+        super(TestBasicMovements, self).__init__(*args, **kwargs)
+
+    def test_0_error_codes(self):
+        e_ok = RLLErrorCode(RLLErrorCode.SUCCESS)
+        e_invalid = RLLErrorCode(RLLErrorCode.INVALID_INPUT)
+        e_recoverable = RLLErrorCode(RLLErrorCode.RECOVERABLE_FAILURE)
+        e_critical = RLLErrorCode(RLLErrorCode.CRITICAL_FAILURE)
+
+        def check_code(error_code, success, failure, recoverable, invalid):
+            self.assertEquals(error_code.succeeded(), success)
+            self.assertEquals(error_code.failed(), not success)
+            self.assertEquals(error_code.is_critical_failure(), failure)
+            self.assertEquals(error_code.is_recoverable_failure(), recoverable)
+            self.assertEquals(error_code.is_invalid_input(), invalid)
+
+        # basic checks to validate the ErrorCode class
+        check_code(e_ok, True, False, False, False)
+        check_code(e_invalid, False, False, False, True)
+        check_code(e_recoverable, False, False, True, False)
+        check_code(e_critical, False, True, False, False)
+
+    def test_1_pose_outside_workspace(self):
+        goal_pose = Pose()
+        goal_pose.position = Point(1, 1, 1)
+        resp = self.client.move_ptp(goal_pose)
+        self.assertServiceCallFailedWith(resp,
+                                         RLLErrorCode.NO_IK_SOLUTION_FOUND)
+
+    def test_2_move_joints(self):
+        resp = self.client.move_joints(0, 0, 0, 0, 0, 0, 0)
+        self.assertServiceCallSuccess(resp)
+
+        resp = self.client.move_joints([1, 0, 0, 0, 0, 0, 0])
+        self.assertServiceCallSuccess(resp)
+
+        resp = self.client.move_joints((0, 0, 1, 0, 0, 0, 0))
+        self.assertServiceCallSuccess(resp)
+
+        # should be seven values
+        resp = self.client.move_joints(0, 0, 1, 0, 0, 0, )
+        self.assertFalse(resp)
---
+++ rll_move/tests/scripts/test_main.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+#
+# This file is part of the Robot Learning Lab Move Client
+#
+# Copyright (C) 2019 Mark Weinreuter <uieai@student.kit.edu>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import rospy
+from rll_move_client.test import generate_test_callback, run_project_in
+from invalid_movements import TestInvalidMovements
+from basic_movements import TestBasicMovements
+from rll_move_client.client import RLLDefaultMoveClient
+
+if __name__ == "__main__":
+    tests = [('move_basic', TestBasicMovements),
+             ('move_invalid', TestInvalidMovements)]
+
+    execute = generate_test_callback("rll_move", tests)
+
+    # setup a regular move client and run the tests in the execute callback
+    rospy.init_node("test_move_iface_client")
+    client = RLLDefaultMoveClient(execute)
+    run_project_in(10)
+    rospy.spin()
